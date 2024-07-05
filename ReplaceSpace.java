df3a17a71e5ebb5b533d92e0135db6b0909ea1e0
iftekhar0786 <mdiftekhar.ktr123@gmail.com>
2024-07-05 12:09:27 +0530

    replace space

---
+++ ReplaceSpace.java
@@ -0,0 +1,12 @@
+package com.te.logical.string;
+
+public class ReplaceSpace {
+
+	public static void main(String[] args) {
+		
+		String name = "md iftekha alam";
+		
+		String replaceStr = name.replace(' ', '-');
+		System.out.println(replaceStr);
+	}
+}
